b743b0a3a9fcff2143bd763779e86b5221210e85
Lucas Huber <lucas.huber@org-it.ch>
2016-01-26 17:23:57 +0100

    [MIGRATION] migrate views to version 9.0

--- project_scrum/__openerp__.py
+++ project_scrum/__openerp__.py
@@ -7,14 +7,15 @@
 
 {
     'name': 'Project Scrum',
-    'version': '1.6',
+    'version': '1.7',
     'category': 'Project Management',
+    'summary': 'SCRUM framework integrated into Odoo Project',
     'description': """
 Using Scrum to plan the work in a team.
 =========================================================================================================
-
 More information:
     """,
+
     'author': 'Vertel AB',
     'website': 'http://www.vertel.se',
     'depends': ['project', 'mail'],
@@ -26,8 +27,12 @@ More information:
     #'external_dependencies': {
         #'python' : ['bs4'],
     #},
-    'demo': ['project_scrum_demo.xml'],
+   # 'demo': ['project_scrum_demo.xml'],
     'installable': True,
+    'auto_install': False,
+    'application': True,
 }
+
+
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
